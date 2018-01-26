c77f2347621c8cc5d1b9af6843ab7cff2debbdd0
URIZ <uriz@il.ibm.com>
2018-01-26 18:43:20 +0200

    change port num

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.set('port', (process.env.PORT || 8000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
