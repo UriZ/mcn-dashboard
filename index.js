5cbcd9ed7c55dd9c649ac0e1385c17b34cd352b8
URIZ <uriz@il.ibm.com>
2018-01-28 18:22:56 +0200

    add passport support - test backend api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let passport = require('passport');
 let app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,6 +14,26 @@ app.get('/', function(request, response) {
     response.render('pages/index')
 });
 
+
+let FacebookTokenStrategy = require('passport-facebook-token');
+
+passport.use(new FacebookTokenStrategy({
+        clientID: "1151370004993163",
+        clientSecret: "50bb09be87258f04b79883ddb4655512"
+    }, function(accessToken, refreshToken, profile, done) {
+        return done(error, "ok");
+
+    }
+));
+
+
+app.post('/auth/facebook/token',
+    passport.authenticate('facebook-token'),
+    function (req, res) {
+        // do something with req.user
+        res.send(req.user? 200 : 401);
+    }
+);
 
 
 app.listen(app.get('port'), function() {
